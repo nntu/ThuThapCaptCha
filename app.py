bf8494f9f3a02acdb387833a681a78e9dace146f
Nguyen Ngoc Tu <tumickey@live.com>
2024-10-29 15:57:15 +0700

    fix

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import shutil
 
 import requests
 
-
+from base64 import b64encode
 
 # App config.
 DEBUG = True
@@ -14,10 +14,10 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 @app.route('/')
 def index():
     url = 'https://tracuunnt.gdt.gov.vn/tcnnt/captcha.png?uid=d436b815-5ad2-497c-adb9-33fffddc2f19'
-    response = requests.get(url, stream=True)
-    print(response)
-    
-    return render_template('index.html')
+    response = requests.get(url, stream=True, verify=False)
+    encode = b64encode(response.content)
+    iur = "data:%s;base64,%s" % ("image/png",encode)
+    return render_template('index.html',imag_data = iur)
 
 
 if __name__ == "__main__":
